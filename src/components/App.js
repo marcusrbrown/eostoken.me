5ea33776f47592fc7d316e591cfdd1017f25dafa
Marcus R. Brown <me@igetgam.es>
2018-03-10 19:05:21 -0700

    Add disclaimer.

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import GrommetApp from 'grommet/components/App';
 import Article from 'grommet/components/Article';
 import Header from 'grommet/components/Header';
+import Notification from 'grommet/components/Notification';
+import Paragraph from 'grommet/components/Paragraph';
 import Title from 'grommet/components/Title';
 
 class App extends Component {
   render() {
     return (
       <GrommetApp>
-        <Header>
-          <Title>eostoken.me</Title>
-        </Header>
         <Article>
-          eostoken.me is an appplication for interacting with the EOS ERC-20 token.
+          <Header>
+            <Title>eostoken.me</Title>
+          </Header>
+          <Paragraph>
+            A set of tools for interacting with the EOS ERC-20 token.
+          </Paragraph>
+          <Notification
+            status='warning'
+            message='This website is not affiliated with block.one or the EOS project.'
+          />
         </Article>
       </GrommetApp>
     );
